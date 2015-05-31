b02226a9f14fab266ab453aaa06fad2822902388
patrickfatrick <patrick.fricano@icloud.com>
2015-05-31 10:25:49 -0600

    Compressing styles.css for production

--- config.rb
+++ config.rb
@@ -3,4 +3,4 @@ require 'breakpoint'
 css_dir = './public/stylesheets'
 sass_dir = './public/stylesheets'
 javascripts_dir = "./public/javascripts"
-output_style = :expanded
+output_style = :compressed
