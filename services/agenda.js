9681e514f46a7e3bd0d50953380c329151a42381
patrickfatrick <patrick.fricano@icloud.com>
2015-08-19 21:30:26 -0600

    Agenda testing

--- services/agenda.js
+++ services/agenda.js
@@ -1,16 +1,18 @@
-var async = require('async');
+//var async = require('async');
 var config = require('../config');
-var emailService = require('./email-service');
+//var emailService = require('./email-service');
 var Agenda = require('agenda');
 var agenda = new Agenda(config.agendaOptions);
 
 agenda.define('Agenda running', function (job, done) {
 	var data = job.attrs.data;
-	console.log('Agenda running at ' + data.time);
+	console.log('Agenda running at ' + new Date());
 	done();
 });
 
-agenda.cancel({name: 'Agenda running'}, function (err, numRemoved) {
+agenda.every('3 minutes', 'Agenda running');
+
+/*agenda.cancel({name: 'Agenda running'}, function (err, numRemoved) {
 	if (err) return console.log(err);
 	console.log('Agendas removed: ' + numRemoved);
 	agenda.now('Agenda running', {time: new Date()});
@@ -20,7 +22,7 @@ agenda.jobs({nextRunAt: {$exists: true, $nin: [null]}}, function (err, jobs) {
 	if (err) return console.log(err);
 	//console.log(jobs);
 	async.each(jobs, function(job, callback) {
-		//console.log(job)...;
+		//console.log(job);
 		agenda.cancel({name: job.attrs.name}, function (err, numRemoved) {
 			console.log('Agenda removed => ' + job.attrs.name);
 			agenda.define(job.attrs.name, function (job, done) {
@@ -38,14 +40,11 @@ agenda.jobs({nextRunAt: {$exists: true, $nin: [null]}}, function (err, jobs) {
 				date: job.attrs.data.date
 			});
 			console.log('Agenda recreated => ' + job.attrs.name);
-		/*job.schedule(job.attrs.nextRunAt);
-		console.log('Agenda rescheduled: ' + job.attrs.name + ' for ' + job.attrs.nextRunAt);
-		job.save();*/
 		}, function (err) {
 			if (err) return console.log(err);
 			console.log('All agendas recreated successfully');
 		});
 	});
-});
+});*/
 
 module.exports = agenda;
