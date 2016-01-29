18fa54e23c47329ab602c8e9ad4cf6c271c5624b
patrickfatrick <patrick.fricano@icloud.com>
2016-01-29 06:58:27 -0700

    small style change to buttons

---
+++ test/components-spec/menu-spec/menu-components-spec/Darkmode.spec.js
@@ -0,0 +1,59 @@
+/* global it describe sinon */
+import chai from 'chai'
+import Vue from 'vue'
+import Darkmode from '../../../../public/components/menu/menu-components/Darkmode.vue'
+
+chai.should()
+describe('Darkmode.vue', function () {
+  it('should inherit the darkmode property from the state', () => {
+    Darkmode.computed.darkmode().should.equal(false)
+  })
+
+  it('should have a setDarkmode method', () => {
+    Darkmode.methods.setDarkmode.should.be.an.instanceof(Function)
+  })
+
+  it('should render with initial state', () => {
+    const vm = new Vue({
+      template: '<div><test></test></div>',
+      components: {
+        'test': Darkmode
+      }
+    }).$mount()
+
+    vm.$el.querySelector('.fa').classList.contains('fa-moon-o').should.be.true
+  })
+
+  it('should respond to changes in the state', (done) => {
+    sinon.stub(Darkmode.computed, 'darkmode').returns(true)
+    const vm = new Vue({
+      template: '<div><test></test></div>',
+      components: {
+        'test': Darkmode
+      }
+    }).$mount()
+
+    vm.$el.querySelector('.fa').classList.contains('fa-sun-o').should.be.true
+
+    Darkmode.computed.darkmode.restore()
+    done()
+  })
+
+  it('should call setDarkmode on button click', (done) => {
+    sinon.stub(Darkmode.computed, 'darkmode').returns(false)
+    const vm = new Vue({
+      template: '<div><test></test></div>',
+      components: {
+        'test': Darkmode
+      }
+    }).$mount()
+    sinon.spy(vm.$children[0], 'setDarkmode')
+
+    vm.$el.querySelector('#dark-mode').click()
+    vm.$children[0].setDarkmode.calledWith(true).should.be.true
+
+    Darkmode.computed.darkmode.restore()
+    vm.$children[0].setDarkmode.restore()
+    done()
+  })
+})
