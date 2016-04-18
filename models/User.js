28d12b0c12b6dcbc0879e80297dce918fbd967cf
patrickfatrick <patrick.fricano@icloud.com>
2016-04-18 00:37:22 -0600

    resetDate

--- models/User.js
+++ models/User.js
@@ -7,7 +7,7 @@ const User = thinky.createModel('users', {
   dateModified: type.date(),
   id: type.string(),
   key: type.string(),
-  resetDate: type.string(),
+  resetDate: type.date(),
   resetToken: type.string(),
   tasks: [
     {
