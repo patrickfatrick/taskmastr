5536295fc4e7d0643cf7fd513e7d8be66bb93d59
Patrick Fricano <patrick.fricano@icloud.com>
2017-03-12 17:07:57 -0600

    model style fixes

--- src/components/forms/form-components/ForgotPassword.vue
+++ src/components/forms/form-components/ForgotPassword.vue
@@ -1,8 +1,23 @@
 <template>
   <div id="forgot-password">
-    <input id="forgot" class="check" name="forgot" type="checkbox" tabindex="-1" :value="forgot"></input>
-    <button title="Forgot Password" type="button" @click.prevent="toggleForgot(!forgot)">
-      <i class="fa" :class="{'fa-check-square-o': forgot, 'fa-square-o': !forgot}"></i>
+    <input
+      id="forgot"
+      class="check"
+      name="forgot"
+      type="checkbox"
+      tabindex="-1"
+      :value="forgot"
+      @change.prevent="toggleForgot(!forgot)"
+    />
+    <button
+      title="Forgot Password"
+      type="button"
+      @click.prevent="toggleForgot(!forgot)"
+    >
+      <i
+        class="fa"
+        :class="{'fa-check-square-o': forgot, 'fa-square-o': !forgot}"
+      />
     </button>
     <label for="forgot">Forgot your password?</label>
   </div>
--- src/components/forms/form-components/RememberMe.vue
+++ src/components/forms/form-components/RememberMe.vue
@@ -1,8 +1,23 @@
 <template>
   <div id="remember">
-    <input id="remember-me" class="check" name="remember-me" type="checkbox" tabindex="-1" :value="rememberMe"></input>
-    <button title="Remember Me" type="button" @click.prevent="setRememberMe(!rememberMe)">
-      <i class="fa" :class="{'fa-check-square-o': rememberMe, 'fa-square-o':!rememberMe}"></i>
+    <input
+      id="remember-me"
+      class="check"
+      name="remember-me"
+      type="checkbox"
+      tabindex="-1"
+      :value="rememberMe"
+      @change.prevent="setRememberMe(!rememberMe)"
+    />
+    <button
+      title="Remember Me"
+      type="button"
+      @click.prevent="setRememberMe(!rememberMe)"
+    >
+      <i
+        class="fa"
+        :class="{'fa-check-square-o': rememberMe, 'fa-square-o':!rememberMe}"
+      />
     </button>
     <label for="remember-me">Remember me</label>
   </div>
